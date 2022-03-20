e5aca5ecc0ebd882cdb3b9c62482dde9d688b15b
Hweemyoung <legolass93@naver.com>
2022-03-20 23:13:25 +0900

    updated

---
+++ dist/1131.js
@@ -0,0 +1,45 @@
+"use strict";
+/* 1131. Maximum of Absolute Value Expression
+Medium
+
+Given two arrays of integers with equal lengths, return the maximum value of:
+
+|arr1[i] - arr1[j]| + |arr2[i] - arr2[j]| + |i - j|
+
+where the maximum is taken over all 0 <= i, j < arr1.length.
+
+Example 1:
+
+Input: arr1 = [1,2,3,4], arr2 = [-1,4,5,6]
+Output: 13
+
+Example 2:
+
+Input: arr1 = [1,-2,-5,0,10], arr2 = [0,-2,-1,-7,-4]
+Output: 20
+
+Constraints:
+
+    2 <= arr1.length == arr2.length <= 40000
+    -10^6 <= arr1[i], arr2[i] <= 10^6
+
+ */
+function maxAbsValExpr(arr1, arr2) {
+    const len = arr1.length;
+    let max = 0;
+    for (let i = 0; i < len; i++) {
+        for (let j = 0; j <= i; j++) {
+            const val = Math.abs(arr1[i] - arr1[j]) + Math.abs(arr2[i] - arr2[j]) + i - j;
+            if (val > max)
+                max = val;
+        }
+    }
+    return max;
+}
+;
+// function test() {
+//     // const arr1 = [1, 2, 3, 4], arr2 = [-1, 4, 5, 6]; // 13
+//     const arr1 = [1, -2, -5, 0, 10], arr2 = [0, -2, -1, -7, -4]; // 20
+//     console.log(maxAbsValExpr(arr1, arr2));
+// }
+//# sourceMappingURL=1131.js.map
---
+++ src/400.ts
@@ -0,0 +1,53 @@
+/* 400. Nth Digit
+Medium
+
+Given an integer n, return the nth digit of the infinite integer sequence [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ...].
+
+ 
+
+Example 1:
+
+Input: n = 3
+Output: 3
+
+Example 2:
+
+Input: n = 11
+Output: 0
+Explanation: The 11th digit of the sequence 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ... is a 0, which is part of the number 10.
+
+ 
+
+Constraints:
+
+    1 <= n <= 231 - 1
+
+ */
+
+function findNthDigit(n: number): number {
+    // 1 * 9 = 1 * (10 ** 1 - 10 ** 0) // 1~9
+    // 2 * 90 = 2 * (10 ** 2 - 10 ** 1) // 10~99
+    // 3 * 900 = 3 * (10 ** 3 - 10 ** 2) // 100~999
+    // nDigit * (10 ** nDigit - 10 ** (nDigit - 1)) = nDigit * 9 * 10 ** (nDigit - 1)
+    let nDigit = 0;
+    while (true) {
+        nDigit++;
+        n = n - nDigit * 9 * 10 ** (nDigit - 1);
+        if (n < 0) {
+            n = -n;
+            const ceil = 10 ** nDigit;
+            const quotient = Math.floor(n / nDigit);
+            const remainder = n % nDigit;
+            const target = ceil - quotient - 1;
+            const targetStr = target.toString();
+            return parseInt(targetStr[targetStr.length - 1 - remainder]);
+        }
+    }
+};
+
+// function test() {
+//     let n;
+//     // n = 11; // 0
+//     n = 190; // 1
+//     console.log(findNthDigit(n));
+// }
