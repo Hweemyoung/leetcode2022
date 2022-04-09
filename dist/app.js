8d9dd503cfe011f51210b397cf29282cb25cdd1d
Hweemyoung <legolass93@naver.com>
2022-04-10 01:04:39 +0900

    updated

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,2 @@
-import * as Util from "./util.js";
-const a = {
-    b: 2,
-};
-Util.assertEqual(true, false);
-Util.assertEqual(a, Object.assign({}, a));
+"use strict";
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++
@@ -1,7 +0,0 @@
-import * as Util from "./util.js";
-const a = {
-    b: 2,
-};
-
-Util.assertEqual(true, false);
-Util.assertEqual(a, Object.assign({}, a));
