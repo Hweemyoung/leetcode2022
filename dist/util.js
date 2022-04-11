47ad7a5ae98d4eb2db162d3d2c496a11e7da7034
Hweemyoung Kim <legolass93@naver.com>
2022-04-12 01:07:22 +0900

    .gitignore working

--- dist/util.js
+++
@@ -1,5 +0,0 @@
-export function assertEqual(evaluate, expect) {
-    if (evaluate !== expect)
-        console.error(`Assertion error:\nEvaluating `, evaluate, `but expected `, expect);
-}
-//# sourceMappingURL=util.js.map
